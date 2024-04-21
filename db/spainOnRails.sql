9ff290e1ed481b6f9ac220c83b5a01045f447941
abrullc <adrianbrullcontreras@gmail.com>
2024-04-21 14:24:35 +0200

    Configuración inicial del proyecto

---
+++ db/spainOnRails.sql
@@ -0,0 +1,151 @@
+-- -----------------------------------------------------
+-- Schema "spain_on_rails"
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS spain_on_rails;
+CREATE SCHEMA IF NOT EXISTS spain_on_rails DEFAULT CHARACTER SET utf8;
+USE spain_on_rails;
+
+-- -----------------------------------------------------
+-- Table "tren"
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS tren (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  nombre VARCHAR(45) UNIQUE NOT NULL,
+  capacidad INT NOT NULL
+);
+
+
+-- -----------------------------------------------------
+-- Table "ruta"
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS ruta (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  tren_id INT,
+  origen VARCHAR(45) NOT NULL,
+  destino VARCHAR(45) NOT NULL,
+  salida DATETIME NOT NULL,
+  llegada DATETIME NOT NULL,
+  CONSTRAINT fk_ruta_tren
+    FOREIGN KEY (tren_id)
+    REFERENCES tren (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
+
+-- -----------------------------------------------------
+-- Table "estacion"
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS estacion (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  ruta_id INT NOT NULL,
+  nombre VARCHAR(100) NOT NULL,
+  poblacion VARCHAR(100) NOT NULL,
+  direccion VARCHAR(150) NOT NULL,
+  longitud FLOAT NOT NULL,
+  latitud FLOAT NOT NULL,
+  CONSTRAINT fk_estacion_ruta
+    FOREIGN KEY (ruta_id)
+    REFERENCES ruta (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
+
+
+-- -----------------------------------------------------
+-- Table "punto_interes"
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS punto_interes (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  estacion_id INT NOT NULL,
+  nombre VARCHAR(100) NOT NULL,
+  direccion VARCHAR(150) NOT NULL,
+  longitud FLOAT NOT NULL,
+  latitud FLOAT NOT NULL,
+  CONSTRAINT fk_punto_interes_estacion
+    FOREIGN KEY (estacion_id)
+    REFERENCES estacion (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
+
+
+-- -----------------------------------------------------
+-- Table "usuario"
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS usuario (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  username VARCHAR(45) UNIQUE NOT NULL,
+  password VARCHAR(45) NOT NULL,
+  email VARCHAR(100) NULL
+);
+
+
+-- -----------------------------------------------------
+-- Table "plan_viaje"
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS plan_viaje (
+  id INT PRIMARY KEY AUTO_INCREMENT
+);
+
+
+-- -----------------------------------------------------
+-- Table "pasaje"
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS pasaje (
+  ruta_id INT NOT NULL,
+  usuario_id INT NOT NULL,
+  fecha DATETIME NOT NULL,
+  precio FLOAT NOT NULL,
+  habitacion VARCHAR(20) NOT NULL,
+  CONSTRAINT multiple_primary_key_pasaje PRIMARY KEY (ruta_id, usuario_id),
+  CONSTRAINT fk_ruta_pasaje
+    FOREIGN KEY (ruta_id)
+    REFERENCES ruta (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_usuario_pasaje
+    FOREIGN KEY (usuario_id)
+    REFERENCES usuario (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
+
+
+-- -----------------------------------------------------
+-- Table "plan_viaje_usuario"
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS plan_viaje_usuario (
+  plan_viaje_id INT NOT NULL,
+  usuario_id INT NOT NULL,
+  CONSTRAINT multiple_primary_key_plan_viaje_usuario PRIMARY KEY (plan_viaje_id, usuario_id),
+  CONSTRAINT fk_plan_viaje_usuario_plan_viaje
+    FOREIGN KEY (plan_viaje_id)
+    REFERENCES plan_viaje (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_plan_viaje_usuario_usuario
+    FOREIGN KEY (usuario_id)
+    REFERENCES usuario (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
+
+
+-- -----------------------------------------------------
+-- Table "plan_viaje_punto_interes"
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS plan_viaje_punto_interes (
+  plan_viaje_id INT NOT NULL,
+  punto_interes_id INT NOT NULL,
+  CONSTRAINT multiple_primary_key_plan_viaje_punto_interes PRIMARY KEY (plan_viaje_id, punto_interes_id),
+  CONSTRAINT fk_plan_viaje_punto_interes_plan_viaje
+    FOREIGN KEY (plan_viaje_id)
+    REFERENCES plan_viaje (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_plan_viaje_punto_interes_punto_interes
+    FOREIGN KEY (punto_interes_id)
+    REFERENCES punto_interes (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
